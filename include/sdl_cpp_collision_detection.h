9ce9236277b207b7798e5678be3c99e97d7d80f2
Byran Wills-Heath <byran@adgico.co.uk>
2017-06-01 23:44:17 +0100

    Changed collision to not be templated

--- include/sdl_cpp_collision_detection.h
+++ include/sdl_cpp_collision_detection.h
@@ -6,31 +6,10 @@
 namespace sdl
 {
     // checks if a point is inside a (clockwise defined) triangle
-    template<typename T>
-    bool tri_point_collision(T const& p0, T const& p1, T const& p2, T const& toCheck)
-    {
-        using U = decltype(T::x);
+    bool tri_point_collision(SDL_Point const& p0, SDL_Point const& p1, SDL_Point const& p2,
+                             SDL_Point const& toCheck);
 
-        U const nx0 = p0.y - p1.y;
-        U const ny0 = -(p0.x - p1.x);
-
-        U const nx1 = p1.y - p2.y;
-        U const ny1 = -(p1.x - p2.x);
-
-        U const nx2 = p2.y - p0.y;
-        U const ny2 = -(p2.x - p0.x);
-
-        U const d0 = nx0 * (p0.x - toCheck.x) + ny0 * (p0.y - toCheck.y);
-        U const d1 = nx1 * (p1.x - toCheck.x) + ny1 * (p1.y - toCheck.y);
-        U const d2 = nx2 * (p2.x - toCheck.x) + ny2 * (p2.y - toCheck.y);
-
-        U const zero{0};
-
-        return ((d0 < zero) && (d1 < zero) && (d2 < zero));
-    }
-
-    template<typename T>
-    bool tri_point_collision(T const* points, T const& toCheck)
+    inline bool tri_point_collision(SDL_Point const* points, SDL_Point const& toCheck)
     {
         return tri_point_collision(points[0], points[1], points[2], toCheck);
     }
