0b3af42d8e7556014fe7f73f7f462520ef93ce1b
Byran Wills-Heath <byran@adgico.co.uk>
2020-08-06 17:45:21 +0100

    Add executable path on Windows

--- src/widgets/executable_path_windows.cpp
+++ src/widgets/executable_path_windows.cpp
@@ -1,11 +1,15 @@
+#include <sdl_cpp/widgets/executable_path.h>
+#include <string>
+#include "Windows.h"
+//#error Still to implement
 
-#error Still to implement
+using namespace ::std;
 
 string executable_path()
 {
 	char buffer[16 * 1024];
 
-	int bytes = GetModuleFileName(NULL, buffer, sizeof(buffer));
+	int bytes = GetModuleFileName(nullptr, buffer, sizeof(buffer));
 	if (bytes == 0)
 	{
 		throw unable_to_find_executable_path{};
@@ -15,5 +19,5 @@ string executable_path()
 
 	return std::string{executable_path.cbegin(),
 					   executable_path.cbegin() +
-					   executable_path.find_last_of('/')};
+					   executable_path.find_last_of('\\')};
 }
