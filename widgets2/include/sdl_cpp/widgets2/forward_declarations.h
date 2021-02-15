ad44d5d759dea5ccbed5a837405478cf21553f83
Byran Wills-Heath <byran@adgico.co.uk>
2021-02-15 21:48:47 +0000

    Add forward declarations for widgets2

---
+++ widgets2/include/sdl_cpp/widgets2/forward_declarations.h
@@ -0,0 +1,16 @@
+#ifndef CSOUNDGUI_FORWARD_DECLARATIONS_H
+#define CSOUNDGUI_FORWARD_DECLARATIONS_H
+
+namespace sdl
+{
+	namespace widgets2
+	{
+		class application;
+		class panel;
+		class widget;
+		class widget_container;
+		class window;
+	}
+}
+
+#endif //CSOUNDGUI_FORWARD_DECLARATIONS_H
