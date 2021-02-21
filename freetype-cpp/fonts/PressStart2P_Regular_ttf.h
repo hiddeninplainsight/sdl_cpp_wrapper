786e1311e0ac0f6a6affd8749d0dd3faad3be9be
Byran Wills-Heath <byran@adgico.co.uk>
2021-02-21 19:43:37 +0000

    Add PressStart2P in memory font

---
+++ freetype-cpp/fonts/PressStart2P_Regular_ttf.h
@@ -0,0 +1,7 @@
+#ifndef FREETYPE_CPP_FONTS_PRESSSTART2P_REGULAR_TTF_H
+#define FREETYPE_CPP_FONTS_PRESSSTART2P_REGULAR_TTF_H
+
+extern unsigned char const PressStart2P_Regular_ttf[]
+unsigned int const PressStart2P_Regular_ttf_len = 118204;
+
+#endif  // FREETYPE_CPP_FONTS_PRESSSTART2P_REGULAR_TTF_H
